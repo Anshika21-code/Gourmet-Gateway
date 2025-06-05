eb9e414e4d3ce112ff715d305b5e3b9a6138b4c5
Anshika Tank <anshikatank2110@gmail.com>
2025-06-05 22:30:47 +0530

    Fixed import path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Home from './pages/Home/Home';
 import Cart from './pages/Cart/Cart';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Footer from './components/Footer/Footer'; // Assuming Footer is imported from here
-import LoginPopup from './components/LoginPopup/LoginPopu';
+import LoginPopup from './components/LoginPopup/LoginPopup';
 import { useState } from 'react';
 
 const App = () => {
